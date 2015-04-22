903b62642c0d2f95430e416d9bd3deed608870f1
高帅 <jn.gaoshuai@gmail.com>
2015-04-22 17:04:49 +0800

    高帅测试一下

---
+++ gaoshuaiGitTest/src/main/java/com/gaoshuai/App.java
@@ -0,0 +1,13 @@
+package com.gaoshuai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
